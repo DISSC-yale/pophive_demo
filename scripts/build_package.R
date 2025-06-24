dcf202b8b33c74fe9a2b6e156a45245a3ad953e2
Micah <micah.iserman@gmail.com>
2025-06-24 18:28:22 -0400

    adds pophive_standardize_epic

--- scripts/build_package.R
+++ scripts/build_package.R
@@ -6,6 +6,17 @@ pkgdown::build_site(lazy = TRUE)
 devtools::check()
 covr::report(covr::package_coverage(quiet = FALSE), "docs/coverage.html")
 
+# update sysdata.rda
+population <- pophive::pophive_load_census(2021, "resources")
+epic_id_maps <- list(
+  regions = structure(population$GEOID, names = population$region_name),
+  months = structure(
+    formatC(seq_len(12), width = 2L, flag = "0"),
+    names = month.abb
+  )
+)
+save(epic_id_maps, file = "r/sysdata.rda", compress = "xz")
+
 # update lock file if needed
 
 ## from ingestion files
--- tests/testthat/test-pophive_epic.R
+++ tests/testthat/test-pophive_epic.R
@@ -5,14 +5,14 @@ test_that("reading works", {
 
   path <- paste0(stage_dir, "/RSV.csv")
   raw_lines <- c(
-    "metadata field,metadata value,,",
-    ",,,",
-    ",,Measures,Value Name",
-    "Year,Age,RSV,",
-    "2020,6 years or more,m1,1",
-    ",Total: all,m2,2",
-    "2021,65 years or more,m1,3",
-    ",No value,m2,4"
+    "metadata field,metadata value,,,,",
+    ",,,,,",
+    ",,,,Measures,Value Name",
+    "county,Year,week,Age,RSV,",
+    '"SALEM, VA",2020,Jun 6,6 years or more,m1,1',
+    '"SALEM, VA",,,Total: all,m2,2',
+    '"SALEM, VA",2021,Jun 6,65 years or more,m1,3',
+    '"SALEM, VA",,,No value,m2,4'
   )
   writeLines(raw_lines, path)
   writeLines(raw_lines, paste0(stage_dir, "/RSV2.csv"))
@@ -22,10 +22,12 @@ test_that("reading works", {
   expect_identical(
     as.character(unlist(read$data$rsv)),
     c(
+      rep("SALEM, VA", 4L),
       "2020",
       "2020",
       "2021",
       "2021",
+      rep("Jun 6", 4L),
       "6+ Years",
       "Total",
       "65+ Years",
@@ -36,5 +38,15 @@ test_that("reading works", {
       "m2",
       1L:4L
     )
+  )
+
+  standard <- pophive_standardize_epic(read$data$rsv)
+  expect_identical(
+    colnames(standard),
+    c("geography", "time", "age", "rsv", "value_name")
+  )
+  expect_identical(
+    as.character(standard[1L, ]),
+    c("51775", "2020-06-06", "6+ Years", "m1", "1")
   )
 })
